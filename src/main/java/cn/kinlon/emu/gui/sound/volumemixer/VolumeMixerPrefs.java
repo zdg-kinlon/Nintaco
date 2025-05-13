2431dfec3194e694dc69897fe394de827ae884dc
紫蝶冰澜 <32889615+zdg-kinlon@users.noreply.github.com>
2025-05-14 00:10:23 +0800

    主音量默认100%

--- src/main/java/cn/kinlon/emu/gui/sound/volumemixer/VolumeMixerPrefs.java
+++ src/main/java/cn/kinlon/emu/gui/sound/volumemixer/VolumeMixerPrefs.java
@@ -59,7 +59,7 @@ public class VolumeMixerPrefs implements Serializable {
     public int getMasterVolume() {
         synchronized (AppPrefs.class) {
             if (masterVolume == null) {
-                masterVolume = 10;
+                masterVolume = 100;
             }
             return masterVolume;
         }
